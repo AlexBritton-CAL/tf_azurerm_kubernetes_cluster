419e63a48e10d772154e9c82a3337020aa244215
AlexBritton-CAL <alex.britton@calastone.com>
2025-09-16 23:18:16 +0100

    extra parameters

--- identity.tf
+++ identity.tf
@@ -1,3 +1,7 @@
+locals {
+  vnet_name = var.global_config.spoke.network.virtual_network_name
+  vnet_resource_group_name = var.global_config.spoke.network.virtual_network_resource_group_name
+}
 resource "azurerm_user_assigned_identity" "kubelet_identity" {
   location            = try(local.kubernetes_cluster.location, var.global_config.global.location)
   name                = "${local.kubernetes_cluster_name}-kubeletid"
@@ -16,3 +20,35 @@ resource "azurerm_role_assignment" "cluster_kubelet_id" {
   scope                            = azurerm_user_assigned_identity.kubelet_identity.id
   skip_service_principal_aad_check = true
 }
+
+data "azurerm_private_dns_zone" "aks" {
+  name                = "privatelink.${azurerm_resource_group.this.location}.azmk8s.io"
+  resource_group_name = var.config.privatelink_dns_zones.rg_name
+  provider            = azurerm.privatelink_dns
+}
+
+data "azurerm_virtual_network" "this" {
+  name                = var.config.vnet_name
+  resource_group_name = var.config.vnet_rg_name
+}
+
+# resource "azurerm_role_assignment" "cluster_kubelet_id" {
+#   principal_id                     = azurerm_user_assigned_identity.cluster_identity.principal_id
+#   role_definition_name             = "Managed Identity Operator"
+#   scope                            = azurerm_user_assigned_identity.cluster_kubelet_id.id
+#   skip_service_principal_aad_check = true
+# }
+
+# resource "azurerm_role_assignment" "cluster_vnet" { # Required to deploy istio internal load balancer
+#   principal_id                     = azurerm_user_assigned_identity.cluster_identity.principal_id
+#   role_definition_name             = "Contributor"
+#   scope                            = data.azurerm_virtual_network.this.id
+#   skip_service_principal_aad_check = true
+# }
+
+# resource "azurerm_role_assignment" "cluster_aks_cluster_dns_zone" {
+#   principal_id                     = azurerm_user_assigned_identity.cluster_identity.principal_id
+#   role_definition_name             = "Private DNS Zone Contributor"
+#   scope                            = data.azurerm_private_dns_zone.aks.id
+#   skip_service_principal_aad_check = true
+# }
--- module_defaults.tf
+++ module_defaults.tf
@@ -46,8 +46,8 @@ locals {
         vm_size                      = "Standard_B4s_v2"
         temporary_name_for_rotation  = "systemtemp"
         only_critical_addons_enabled = true
-        zones                        = ["1", "2", "3"]                                                                                                                                                             # Deploy across availability zones
-        vnet_subnet_id               = "/subscriptions/${var.global_config.global.subscription_id}/resourceGroups/${var.global_config.global.spoke.network.resource_group_name}/providers/Microsoft.Network/virtualNetworks/${var.global_config.global.spoke.network.virtual_network_name}/subnets/aks-snet" # UPDATE LATER - Provide subnet ID if using existing VNet
+        zones                        = ["1", "2", "3"]  # Deploy across availability zones
+        vnet_subnet_id               = "/subscriptions/${var.global_config.global.subscription_id}/resourceGroups/${var.global_config.global.spoke.network.resource_group_name}/providers/Microsoft.Network/virtualNetworks/${var.global_config.global.spoke.network.virtual_network_name}/subnets/aks-snet"
         node_public_ip_enabled       = false
 
         upgrade_settings = {
