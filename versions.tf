ed070f9338fcbe2f1a20f6a261a283accf67ae9a
AlexBritton-CAL <alex.britton@calastone.com>
2025-09-18 10:40:32 +0100

    extra parameters

--- versions.tf
+++ versions.tf
@@ -3,8 +3,7 @@ terraform {
     azurerm = {
       source                = "hashicorp/azurerm"
       version               = "~> 4.25.0"
-      # configuration_aliases = [azurerm.privatelink_dns, azurerm.public_dns, azurerm.private_dns]
-      configuration_aliases = [azurerm.privatelink_dns]
+      configuration_aliases = [azurerm.privatelink_dns, azurerm.public_dns, azurerm.private_dns]
     }
   }
   required_version = ">= 1.1.0"
