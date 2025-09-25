e6b407e6fa9884164a6b3e5e48cb500e04d549a3
AlexBritton-CAL <alex.britton@calastone.com>
2025-09-25 13:19:50 +0100

    extra parameters

--- module_defaults.tf
+++ module_defaults.tf
@@ -14,7 +14,7 @@ locals {
       private_cluster_enabled    = true
       private_dns_zone_id        = "/subscriptions/${var.global_config.global.privatelink_dns_zones.subscription_id}/resourceGroups/${var.global_config.global.privatelink_dns_zones.resource_group_name}/providers/Microsoft.Network/privateDnsZones/privatelink.uksouth.azmk8s.io"
       dns_prefix_private_cluster = replace("${var.resource_prefix}-${var.instance_name}-aks", "_", "-")
-      dns_prefix                 = "publicaks01" # UPDATE LATER - Provide a unique DNS prefix for public cluster
+      dns_prefix                 = replace(local.kubernetes_cluster_name, "_", "-")
 
       automatic_upgrade_channel = "stable"
 
