0a4d0ea5ce43d8c24628b9e91232b5f7bf1d7fe1
AlexBritton-CAL <alex.britton@calastone.com>
2025-10-10 16:44:30 +0100

    fix RBAC

--- cert_manager.tf
+++ cert_manager.tf
@@ -30,7 +30,7 @@ resource "azurerm_role_assignment" "certmanager_public_dns" {
 
 resource "azurerm_role_assignment" "certmanager_private_dns" {
   principal_id                     = azurerm_user_assigned_identity.certmanager.principal_id
-  role_definition_name             = "Private DNS Zone Contributor"
+  role_definition_name             = "DNS Zone Contributor"
   scope                            = data.azurerm_private_dns_zone.private_dns_zone.id
   skip_service_principal_aad_check = true
 }
