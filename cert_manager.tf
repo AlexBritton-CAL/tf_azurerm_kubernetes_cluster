e0f94d7f99b71926fb3ac833d5cb19b09b3d141f
AlexBritton-CAL <alex.britton@calastone.com>
2025-10-10 17:50:49 +0100

    fix RBAC

--- cert_manager.tf
+++ cert_manager.tf
@@ -9,32 +9,32 @@ resource "azurerm_user_assigned_identity" "certmanager" {
   resource_group_name = var.resource_group_name
 }
 
-# FIXME: Runners need to be able to assign these roles
-# data "azurerm_dns_zone" "public_dns_zone" {
-#   name                = var.global_config.global.public_dns_zone.name
-#   resource_group_name = var.global_config.global.public_dns_zone.resource_group_name
-#   provider            = azurerm.public_dns
-# }
+# FIX ME: Runners need to be able to assign these DNS roles to the identity
+data "azurerm_dns_zone" "public_dns_zone" {
+  name                = var.global_config.global.public_dns_zone.name
+  resource_group_name = var.global_config.global.public_dns_zone.resource_group_name
+  provider            = azurerm.public_dns
+}
 
-# data "azurerm_private_dns_zone" "private_dns_zone" {
-#   name                = var.global_config.global.private_dns_zone.name
-#   resource_group_name = var.global_config.global.private_dns_zone.resource_group_name
-#   provider            = azurerm.private_dns
-# }
+data "azurerm_dns_zone" "private_dns_zone" {
+  name                = var.global_config.global.private_dns_zone.name
+  resource_group_name = var.global_config.global.private_dns_zone.resource_group_name
+  provider            = azurerm.private_dns
+}
 
-# resource "azurerm_role_assignment" "certmanager_public_dns" {
-#   principal_id                     = azurerm_user_assigned_identity.certmanager.principal_id
-#   role_definition_name             = "DNS Zone Contributor"
-#   scope                            = data.azurerm_dns_zone.public_dns_zone.id
-#   skip_service_principal_aad_check = true
-# }
+resource "azurerm_role_assignment" "certmanager_public_dns" {
+  principal_id                     = azurerm_user_assigned_identity.certmanager.principal_id
+  role_definition_name             = "DNS Zone Contributor"
+  scope                            = data.azurerm_dns_zone.public_dns_zone.id
+  skip_service_principal_aad_check = true
+}
 
-# resource "azurerm_role_assignment" "certmanager_private_dns" {
-#   principal_id                     = azurerm_user_assigned_identity.certmanager.principal_id
-#   role_definition_name             = "DNS Zone Contributor"
-#   scope                            = data.azurerm_private_dns_zone.private_dns_zone.id
-#   skip_service_principal_aad_check = true
-# }
+resource "azurerm_role_assignment" "certmanager_private_dns" {
+  principal_id                     = azurerm_user_assigned_identity.certmanager.principal_id
+  role_definition_name             = "DNS Zone Contributor"
+  scope                            = data.azurerm_dns_zone.private_dns_zone.id
+  skip_service_principal_aad_check = true
+}
 
 resource "azurerm_federated_identity_credential" "certmanager" {
   name                = "${azurerm_kubernetes_cluster.this.name}-ServiceAccount-${local.cert-manager-ns}-${local.cert-manager-sa_name}"
