d1f38b14f8c3581e6d26738ce8e3621bcc558d92
AlexBritton-CAL <alex.britton@calastone.com>
2025-12-17 15:02:59 +0000

    add output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output cluster_name {
+  description = "The name of the Kubernetes cluster."
+  value       = azurerm_kubernetes_cluster.this.name
+}
+
 output "oidc_issuer_url" {
   description = "The OIDC issuer URL for the cluster."
   value       = try(azurerm_kubernetes_cluster.this.oidc_issuer_url, null)
