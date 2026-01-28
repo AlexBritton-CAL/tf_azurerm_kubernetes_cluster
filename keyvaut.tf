ee7832f1f62f6a93c88e82744a0efa4da0e26eaa
AlexBritton-CAL <alex.britton@calastone.com>
2026-01-28 16:52:38 +0000

    cert key vault

--- keyvaut.tf
+++ keyvaut.tf
@@ -18,7 +18,7 @@ resource "azurerm_federated_identity_credential" "keyvault" {
   subject             = "system:serviceaccount:${local.keyvault-ns}:${local.keyvault-sa_name}"
 }
 
-data "azrerm_keyvault" "keyvault" {
+data "azurerm_keyvault" "keyvault" {
   name                = var.global_config.global.certificate_keyvault.name
   resource_group_name = var.global_config.global.certificate_keyvault.resource_group_name
 }
