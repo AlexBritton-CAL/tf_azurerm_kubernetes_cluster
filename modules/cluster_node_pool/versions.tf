75820d71786d1467a66a69cfa9dcb80bad573a00
AlexBritton-CAL <alex.britton@calastone.com>
2026-03-03 14:47:30 +0000

    fixes

--- modules/cluster_node_pool/versions.tf
+++ modules/cluster_node_pool/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "~> 4.58.0"
+      version               = "~> 4.62.1"
+  required_version = ">= 1.1.0"
     }
   }
-  required_version = ">= 1.1.0"
 }
