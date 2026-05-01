4190f47f0354aec637cf72da1059f8fc15964396
AlexBritton-CAL <alex.britton@calastone.com>
2026-05-01 11:15:01 +0100

    make crossplane optional

--- main.user_assigned_identity_crossplane.tf
+++ main.user_assigned_identity_crossplane.tf
@@ -4,15 +4,17 @@ locals {
 }
 
 resource "azurerm_user_assigned_identity" "crossplane" {
+  count               = try(var.config.identity.crossplane_identity.enabled, false) == true ? 1 : 0
   location            = local.location
   name                = coalesce(try(var.config.identity.crossplane_identity.name, null), local.module_defaults.identity.crossplane_identity.name)
   resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_federated_identity_credential" "crossplane" {
+  count      = try(var.config.identity.crossplane_identity.enabled, false) == true ? 1 : 0
   name       = "${azurerm_kubernetes_cluster.this.name}-ServiceAccount-${local.crossplane-ns}-${local.crossplane-sa_name}"
   audience   = ["api://AzureADTokenExchange"]
   issuer     = azurerm_kubernetes_cluster.this.oidc_issuer_url
-  parent_id  = azurerm_user_assigned_identity.crossplane.id
+  parent_id  = azurerm_user_assigned_identity.crossplane[0].id
   subject    = "system:serviceaccount:${local.crossplane-ns}:${local.crossplane-sa_name}"
 }
