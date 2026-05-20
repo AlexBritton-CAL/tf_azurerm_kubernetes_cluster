486e8a5a14de89d1069c282975919b2f0b4910d6
AlexBritton-CAL <alex.britton@calastone.com>
2026-05-20 14:39:50 +0100

    fix crossplane conditioanl

--- main.user_assigned_identity_crossplane.tf
+++ main.user_assigned_identity_crossplane.tf
@@ -1,18 +1,18 @@
 locals {
   crossplane-ns      = "crossplane-system"
   crossplane-sa_name = "crossplane-workload-id-sa"
-  crossplane-enabled = try(var.config.identity.crossplane_identity.enabled, true)
+  crossplane-enabled = try(var.config.identity.crossplane_identity.enabled, false)
 }
 
 resource "azurerm_user_assigned_identity" "crossplane" {
-  count               = try(var.config.identity.crossplane_identity.enabled, false) == true ? 1 : 0
+  count               = local.crossplane-enabled ? 1 : 0
   location            = local.location
   name                = coalesce(try(var.config.identity.crossplane_identity.name, null), local.module_defaults.identity.crossplane_identity.name)
   resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_federated_identity_credential" "crossplane" {
-  count      = try(var.config.identity.crossplane_identity.enabled, false) == true ? 1 : 0
+  count      = local.crossplane-enabled ? 1 : 0
   name       = "${azurerm_kubernetes_cluster.this.name}-ServiceAccount-${local.crossplane-ns}-${local.crossplane-sa_name}"
   audience   = ["api://AzureADTokenExchange"]
   issuer     = azurerm_kubernetes_cluster.this.oidc_issuer_url
