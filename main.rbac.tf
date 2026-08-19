8f43af6d61b7d1b266dc20192701b8c95687ea4c
AlexBritton-CAL <alex.britton@calastone.com>
2026-08-19 18:20:36 +0100

    add aks rbac

--- main.rbac.tf
+++ main.rbac.tf
@@ -4,8 +4,8 @@ module "rbac_aks" {
   context       = "nonprod"
   # context = var.config.context
 
-  default_groups  = try(var.global_config.RBAC.global.default_groups, [])
-  elevated_groups = try(var.global_config.RBAC.global.elevated_groups, [])
+  default_groups  = try(var.global_config.global.RBAC.default_groups, [])
+  elevated_groups = try(var.global_config.global.RBAC.elevated_groups, [])
 
   resource_id = "/subscriptions/${var.global_config.global.subscription_id}/resourceGroups/${var.resource_group_name}"
 }
